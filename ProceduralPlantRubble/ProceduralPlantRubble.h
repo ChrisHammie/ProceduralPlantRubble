014625715d6ca91a3142944c143643eed7bb6842
ChrisHammie <Christopher2.Hamilton@live.uwe.ac.uk>
2017-11-23 20:06:06 +0000

    Changes

--- ProceduralPlantRubble/ProceduralPlantRubble.h
+++ ProceduralPlantRubble/ProceduralPlantRubble.h
@@ -1,16 +1,16 @@
 #pragma once
 
 #include <d3d11.h>
-#include <d3dx11.h>
-#include <D3DX10.h>		//Needed as directx11 is an extension of 10 and uses it's macros, funcitons and classes
+//#include <d3dx11.h>
+//#include <D3DX10.h>		//Needed as directx11 is an extension of 10 and uses it's macros, funcitons and classes
 #include <dinput.h>
 #include <DirectXMath.h>
 
 #include <sstream>
 
 #pragma comment (lib, "d3d11.lib")
-#pragma comment (lib, "d3dx11.lib")
-#pragma comment (lib, "d3dx10.lib")
+//#pragma comment (lib, "d3dx11.lib")
+//#pragma comment (lib, "d3dx10.lib")
 #pragma comment (lib, "dinput8.lib")
 #pragma comment (lib, "dxguid.lib")
 
--- ProceduralPlantRubble/shaders.shader
+++ ProceduralPlantRubble/shaders.shader
@@ -4,8 +4,6 @@ cbuffer cbPerObject
 	float4x4 WVP;
 };
 
-
-
 struct VOut
 {
 	float4 position : SV_POSITION;
